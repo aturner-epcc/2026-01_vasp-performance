71d970af10bc69c54ec9fab1209b688e222cd77f
Andy Turner <a.turner@epcc.ed.ac.uk>
2025-12-30 10:34:46 +0000

    Adds ARCHER2 analysis script

---
+++ analysis/get_CdTe_data_ARCHER2.bash
@@ -0,0 +1,27 @@
+
+datadirs="6.5.1-GCC-LibSci 6.5.1-GCC-MKL 6.5.1-GCC-OpenBLAS"
+
+system="ARCHER2"
+
+for dir in $datadirs
+do
+   if [ -f ${dir}.csv ]
+   then
+      rm ${dir}.csv
+   fi
+   python 2026-01_vasp-performance/analysis/cdte_output_file.py CdTe_Hybrid/${dir} ${dir} ${system} ${dir}.csv
+done
+
+
+first=true
+for dir in $datadirs
+do
+   if [ -v first ]
+   then
+      cat ${dir}.csv > CdTe_combined_ARCHER2.csv
+      unset first
+   else
+      tail -n +2 ${dir}.csv >> CdTe_combined_ARCHER2.csv
+   fi
+done
+
